c718ae39effdc39c12e70d7a4ef57fccfcf7bfa9
Will Tekulve <tekulve.will@gmail.com>
2020-01-09 18:44:56 -0500

    Add windows makefile

---
+++ make.bat
@@ -0,0 +1,3 @@
+cargo build --release
+arm-none-eabi-objcopy -O binary target/thumbv6m-none-eabi/release/can-testbed target/thumbv6m-none-eabi/release/can-testbed.bin
+uf2conv-rs target/thumbv6m-none-eabi/release/can-testbed.bin
